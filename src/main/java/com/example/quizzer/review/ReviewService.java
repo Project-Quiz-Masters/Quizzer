bb7f69056961b067bf504aed92fd062e2a5695e0
emalle <e.lucianille@gmail.com>
2025-12-03 18:15:39 +0200

    Fixed bug in the Review Service file

--- src/main/java/com/example/quizzer/review/ReviewService.java
+++ src/main/java/com/example/quizzer/review/ReviewService.java
@@ -1,14 +1,11 @@
 package com.example.quizzer.review;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
 
-    @Autowired
-
-    public Review addReviewToQuiz(Long quizId, Review payload) {
+    public Review addReview(Long quizId, Review payload) {
         throw new UnsupportedOperationException("Unimplemented method 'addReviewToQuiz'");
     }
 }
